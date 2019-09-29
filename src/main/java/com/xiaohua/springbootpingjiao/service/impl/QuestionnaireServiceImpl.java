64d2f31f578763399932c1235c70e4e9a1a17035
xiongbingjun <1975817114@qq.com>
2019-09-29 14:16:05 +0800

    完善问卷列表的删除 下拉查询  联动查询

--- src/main/java/com/xiaohua/springbootpingjiao/service/impl/QuestionnaireServiceImpl.java
+++ src/main/java/com/xiaohua/springbootpingjiao/service/impl/QuestionnaireServiceImpl.java
@@ -1,5 +1,6 @@
 package com.xiaohua.springbootpingjiao.service.impl;
 
+import com.xiaohua.springbootpingjiao.entity.Papers;
 import com.xiaohua.springbootpingjiao.mapper.QuestionnaireMapper;
 import com.xiaohua.springbootpingjiao.service.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,60 @@ import java.util.List;
 public class QuestionnaireServiceImpl  implements QuestionnaireService {
     @Autowired
     public QuestionnaireMapper QuestionnaireMapper;
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinName(String papers_name, int pages, int limit) {
+        return QuestionnaireMapper.selectQuestionnaireinName(papers_name,pages,limit);
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinNameCount(String papers_name) {
+        return QuestionnaireMapper.selectQuestionnaireinNameCount(papers_name);
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinBatch(String batch_id, int pages, int limit) {
+        return QuestionnaireMapper.selectQuestionnaireinBatch(batch_id,pages,limit);
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinBatchCount(String batch_id) {
+        return QuestionnaireMapper.selectQuestionnaireinBatchCount(batch_id);
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinRoleid(String role_id, int pages, int limit) {
+        return QuestionnaireMapper.selectQuestionnaireinRoleid(role_id,pages,limit);
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinRoleidCount(String role_id) {
+        return QuestionnaireMapper.selectQuestionnaireinRoleidCount(role_id);
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinBRCount(String role_id, String batch_id) {
+        return QuestionnaireMapper.selectQuestionnaireinBRCount(role_id,batch_id);
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectQuestionnaireinBR(String role_id, String batch_id, int pages, int limit) {
+        return QuestionnaireMapper.selectQuestionnaireinBR(role_id,batch_id,pages,limit);
+    }
+
+    @Override
+    public boolean deletePapers(Papers c) {
+        int result = QuestionnaireMapper.deletePapers(c);
+        if (result > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<HashMap<String, String>> selectuser() {
+        return QuestionnaireMapper.selectuser();
+    }
 
     @Override
     public List<HashMap<String, String>> selectbatch() {
