f9ae4a1a1bc19883b5d8321a1ff711280712a2ae
zuojie22 <1040448624@qq.com>
2019-09-30 08:41:17 +0800

    指标管理（一级指标类别的添加、指标的删除）

--- src/main/java/com/xiaohua/springbootpingjiao/mapper/TargetMapper.java
+++ src/main/java/com/xiaohua/springbootpingjiao/mapper/TargetMapper.java
@@ -20,10 +20,14 @@ public interface TargetMapper {
 //    查询所有指标
     List<Target> selectAllTarget();
 
-//    查询指定指标的选项
-
+//    查询指标的选项
     List<Options> selectTargetOptions(int target_id);
 
+//    插入之前查询是否存在相同指标类别（hides=1）
+    int selectTheTargetCategory(String target_Name);
 //    插入指标类别
     int insertTargetCategory(String target_Name,Float target_Weight);
+
+//    删除指标
+    int deleteTarget(int target_Id);
 }
--- src/main/java/com/xiaohua/springbootpingjiao/service/TargetService.java
+++ src/main/java/com/xiaohua/springbootpingjiao/service/TargetService.java
@@ -21,6 +21,11 @@ public interface TargetService {
 //    查询指定指标的选项
     List<Options> selectTargetOptions(int target_id);
 
+//    插入指标类别之前查询是否已存在该类别(hides=1)
+    int selectTheTargetCategory(String target_Name);
 //    插入指标类别
     int insertTargetCategory(String target_Name,Float target_Weight);
+
+//    删除指标
+    int deleteTarget(int target_Id);
 }
