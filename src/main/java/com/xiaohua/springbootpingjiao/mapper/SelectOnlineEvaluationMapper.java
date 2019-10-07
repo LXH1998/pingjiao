15fc85be132cbe6c53f3c7a707c574f6ba5b0ec1
cqyhm <32033658@qq.com>
2019-10-07 19:38:34 +0800

    试卷评教和详情

---
+++ src/main/java/com/xiaohua/springbootpingjiao/mapper/SelectOnlineEvaluationMapper.java
@@ -0,0 +1,16 @@
+package com.xiaohua.springbootpingjiao.mapper;
+
+import com.xiaohua.springbootpingjiao.entity.Fraction;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author: luquanlin
+ * @Date: 2019/10/7 16:46
+ * @VERSION: 1.0
+ **/
+@Repository
+public interface SelectOnlineEvaluationMapper {
+    List<Fraction> selectEvaluationAnswers(int rater,int gradeds,int papers_id,int courses_id);
+}
