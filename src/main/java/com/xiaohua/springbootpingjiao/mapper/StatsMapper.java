b4267b2d53646876baf919df81009a48d484e6ea
xiaoyi06 <480927936@qq.com>
2019-10-08 08:54:31 +0800

    评教统计

--- src/main/java/com/xiaohua/springbootpingjiao/mapper/StatsMapper.java
+++ src/main/java/com/xiaohua/springbootpingjiao/mapper/StatsMapper.java
@@ -1,8 +1,11 @@
 package com.xiaohua.springbootpingjiao.mapper;
 
 import com.xiaohua.springbootpingjiao.entity.Departments;
+import com.xiaohua.springbootpingjiao.entity.Scores;
+import com.xiaohua.springbootpingjiao.entity.WaterPojo;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,15 +18,66 @@ import java.util.Map;
 @Repository
 public interface StatsMapper {
     /**
-     * 查询所有院系数量
+     * @param map
+     * @return 整理流水表信息
      */
-    List<Map>  queryDepartmentSize();
+    List<WaterPojo> clearUpFraction(HashMap map);
+    List<HashMap> queryTeacherStats(HashMap map);
+    /**
+     * @param map
+     * @return 查询评教老师评教的科目
+     */
+    List<HashMap> queryTeacherCourse(HashMap map);
+    int insertBatchScore(List<Scores> list);
+    /**
+     * @param map
+     * @return 统计参与评教学院
+     */
+    List<HashMap> queryScoreDepartment(HashMap map);
+    /**
+     * @param map
+     * @return 查询参与评教老师
+     */
+    List<HashMap> queryScoreTeacher(HashMap map);
+    /**
+     * @param map
+     * @return 查询评价人数
+     */
+    List<HashMap> queryScoreMan(HashMap map);
+    /**
+     * @param map
+     * @return 查询老师评价高于平均分的老师
+     */
+    List<HashMap> queryAverageTeacherDayu(HashMap map);
 
     /**
-     * 查询所有在职老师数量
-     *
+     *  生成 好坏饼图
      */
-    List<Map>  querTeacherSize();
+    List<HashMap> queryTeacherDaYu90(HashMap map);
+    List<HashMap> queryTeacherDaYu60XiaoYu90(HashMap map);
+    List<HashMap> queryTeacherXiaoyu60(HashMap map);
 
-    List<Map>  qeryDepartmentCount();
+    /**
+     * 模糊查询院系平均分
+     * @param map
+     * @return
+     */
+    List<HashMap> queryDepartmentLike(HashMap map);
+
+    /**
+     * 模糊查询paperid
+     * @param map
+     * @return
+     */
+    List<HashMap> queryPaperIdLike(HashMap map);
+    /**
+     * 模糊查询batchID
+     * @param map
+     * @return
+     */
+    List<HashMap> queryBatchIdLike(HashMap map);
+    List<HashMap> queryDartpmentName();
+    List<HashMap> queryBatch();
+    List<HashMap> queryPaper();
+    List<HashMap> queryScoreDealits(HashMap map);
 }
--- src/main/java/com/xiaohua/springbootpingjiao/service/StatsService.java
+++ src/main/java/com/xiaohua/springbootpingjiao/service/StatsService.java
@@ -1,12 +1,30 @@
 package com.xiaohua.springbootpingjiao.service;
 
+import com.xiaohua.springbootpingjiao.entity.WaterPojo;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public interface StatsService {
-    /**
-     * 查询所有院系数量
-     */
-    int   queryDepartmentSize();
-    /**
-     * 查询所有在职老师数量
-     */
-    int   querTeacherSize();
+    /**整理流水表信息*/
+    List<WaterPojo> clearUpFraction(String batchId,String papersId);
+    /**统计平价*/
+    int statisticalFraction(List<WaterPojo> list,String batchId,String papers_Id);
+    /**统计参与评教学院*/
+    List<HashMap> queryScoreDepartment(HashMap map);
+    /**查询参与评教老师*/
+    List<HashMap> queryScoreTeacher(HashMap map);
+    /**查询评价人数*/
+    List<HashMap> queryScoreMan(HashMap map);
+    /**查询老师评价高于平均分的老师*/
+    List<HashMap> queryAverageTeacherDayu(HashMap map);
+    /**好坏饼图*/
+    List<HashMap> queryPie(HashMap map);
+    /**模糊查询院系平均分*/
+    List<HashMap> queryDepartmentLike(HashMap map);
+
+    List<HashMap> queryBatch();
+    List<HashMap> queryPaper();
+    List<HashMap> queryScoreDealits(HashMap map);
 }
